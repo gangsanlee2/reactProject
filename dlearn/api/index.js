5ab7572aa4598fd317bfbb50c52349695b6cba9b
gangsanlee <116553043+gangsanlee2@users.noreply.github.com>
2022-12-16 18:17:21 +0900

    12.16.18:20

--- dlearn/api/index.js
+++ dlearn/api/index.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 const server = `http://localhost:8000`
-export const fashion = req => axios.post(`${server}/dlearn/fashion`, req)
+export const getFashion = id => axios.get(`${server}/dlearn/fashion?id=${id}`)
+export const postFashion = req => axios.post(`${server}/dlearn/fashion`, req)
--- dlearn/components/Fashion.jsx
+++ dlearn/components/Fashion.jsx
@@ -1,21 +1,32 @@
-
 import { useState } from "react";
-import { fashion } from "dlearn/api";
+import { getFashion, postFashion } from "dlearn/api";
 
-const Iris = () => {
+const Fashion = () => {
     const [inputs, setInputs] = useState({})
-    const {testNum} = inputs;
+    const {id} = inputs;
 
     const onChange = e => {
         e.preventDefault()
         const {value, name} = e.target
-        setInputs({...inputs, [name]: value})
+        setInputs({...inputs, [name]: value})   //리스트로 감쌌다는 것은 변수라는 의미이다. ex) a = {'b':5,'c':10}   =>  a[b] = 5
     }
-    const onClick = e => {
+    const onGetClick = e => {
         e.preventDefault()
-        const request = {testNum}
+        alert(`선택한 번호 : ${JSON.stringify(id)}`)
+        getFashion(id)
+        .then((res)=>{
+            alert(`찾는 옷의 종류 : ${JSON.stringify(res.data.result)}`)
+        })
+        .catch((err)=>{
+            console.log(err)
+            alert('번호를 다시 입력해주세요')
+        })
+    }
+    const onPostClick = e => {
+        e.preventDefault()
+        const request = {id}
         alert(`선택한 번호 : ${JSON.stringify(request)}`)
-        fashion(request)
+        postFashion(request)
         .then((res)=>{
             console.log(`Response is ${res.data.result}`)
             localStorage.setItem('token', JSON.stringify(res.data.result))
@@ -28,9 +39,14 @@ const Iris = () => {
     }
     
     return(
-    <>
-        번호 선택: <input type="number" name="testNum" onChange={onChange} /><br/>
-        <button onClick={onClick}> 입력 </button>
+    <>  <form method="get">
+        번호 선택: <input type="text" name="id" onChange={onChange} /><br/>
+        <button onClick={onGetClick}> GET 입력 </button><br/>
+        </form>
+        <form method="post">
+        번호 선택: <input type="text" name="id" onChange={onChange} /><br/>
+        <button onClick={onPostClick}> POST 입력 </button>
+        </form>
     </>)
 }
-export default Iris
+export default Fashion
